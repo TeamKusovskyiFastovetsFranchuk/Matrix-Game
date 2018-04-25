c7e5a0e16c2bd8beb3ce97d74b2edc4a32c75a79
Stanislav Kusovsky <kusovsky95@gmail.com>
2018-04-26 02:04:01 +0300

    321-340(fix)

--- task_321_340.cpp
+++ task_321_340.cpp
@@ -1,34 +1,17 @@
 #include "task_321_340.h"
 
-uint32_t setBit(uint32_t source, int n, bool state);
-bool getBit(uint32_t source, int n);
-
 std::string task_321_340(const std::string& str)
 {
     uint32_t value = static_cast<uint32_t>(std::stoul(str));
     uint32_t tmp = value;
 
-    value = setBit(value, 0, getBit(tmp, 1));
-    value = setBit(value, 1, getBit(tmp, 0));
-    value = setBit(value, 2, getBit(tmp, 3));
-    value = setBit(value, 3, getBit(tmp, 2));
+    char* byte_value = reinterpret_cast<char*>(&value);
+    char* byte_tmp = reinterpret_cast<char*>(&tmp);
+
+    byte_value[0] = byte_tmp[1];
+    byte_value[1] = byte_tmp[0];
+    byte_value[2] = byte_tmp[3];
+    byte_value[3] = byte_tmp[2];
 
     return std::to_string(value);
-}
-
-
-uint32_t setBit(uint32_t source, int n, bool state)
-{
-    if (state) {
-        source |= (1 << n);
-    }
-    else {
-        source &= ~(1 << n);
-    }
-    return source;
-}
-
-bool getBit(uint32_t source, int n)
-{
-    return source & (1 << n);
 }
