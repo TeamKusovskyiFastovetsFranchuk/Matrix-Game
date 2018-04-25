37a601b01eb2ffe2752a39a7e4e0c96a8ba6a846
FIvanO <FIvanO1998@gmail.com>
2018-04-25 18:58:31 +0300

    101-120

---
+++ vectorortogonal.h
@@ -0,0 +1,40 @@
+#ifndef VECTORORTOGONAL_H
+#define VECTORORTOGONAL_H
+
+#include <string>
+using std::string;
+using std::stoi;
+using std::to_string;
+
+string isOrtogonal(string input) {
+	int coord[5];
+	int ind = 0;
+	int l = input.length();
+	string s = "";
+	for (int i = 0; i < l; ++i) {
+		if (input[i] == '{') {
+			continue;
+		}
+		if (input[i] == '}') {
+			if (i + 1 == l) {
+				coord[ind] = stoi(s);
+				break;
+			} else {
+				coord[ind] = stoi(s);
+				ind++;
+				s = "";
+				i++;
+				continue;
+			}
+		}
+		if (input[i] == ',') {
+			coord[ind] = stoi(s);
+			ind++;
+			s = "";
+		} else {
+			s += input[i];
+		}
+	}
+	return to_string((coord[0] * coord[2]) + (coord[1] * coord[3]) == 0 ? 1 : 0);
+}
+#endif // VECTORORTOGONAL_H
