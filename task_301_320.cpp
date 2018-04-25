028273d9fac0d6f54bb1fa379c8e5867361ff395
Stanislav Kusovsky <kusovsky95@gmail.com>
2018-04-26 01:56:23 +0300

    301-320(fix)

--- task_301_320.cpp
+++ task_301_320.cpp
@@ -3,7 +3,16 @@
 
 std::string task_301_320(const std::string& str)
 {
-    unsigned __int32 value = static_cast<unsigned __int32>(std::stoul(str));
-    value = ~value;
-    return std::to_string(value);
+    uint32_t value = static_cast<uint32_t>(std::stoul(str));
+    uint32_t result = 0;
+
+    const int COUNT_BITS_UINT32 = sizeof(uint32_t) * 8;
+
+    for (int i = 0; i < COUNT_BITS_UINT32 / 2; i++) {
+        result |= (value & (1 << i)) << (COUNT_BITS_UINT32 - 2 * i - 1);
+    }
+    for (int i = COUNT_BITS_UINT32 / 2; i < COUNT_BITS_UINT32 - 1; i++) {
+        result |= (value & (1 << i)) >> (2 * i - COUNT_BITS_UINT32 + 1);
+    }
+    return std::to_string(result);
 }
