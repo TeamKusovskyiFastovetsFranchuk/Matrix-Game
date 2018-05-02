f935cd38c86203a659f6528813b7c2e58a6aad2a
FIvanO <FIvanO1998@gmail.com>
2018-05-02 12:20:32 +0300

    221-240 (rename)

--- task_221_240.h
+++ task_221_240.h
@@ -6,7 +6,7 @@ using std::string;
 using std::stoi;
 using std::to_string;
 
-string task221(string &input) {
+string task_221(string &input) {
 	int l = input.length();
 	string ans = "0123456789ABCDEF";
 	for (int i = 0; i < l; ++i) {
